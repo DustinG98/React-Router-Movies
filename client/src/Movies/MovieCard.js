7467c3bdf6e73db5cd6493688ae1f5b082255b9a
DustinG98 <thedustingraham@gmail.com>
2019-11-12 14:09:42 -0700

    Added save button to home page and individual movie cards

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MovieCard = (props) => {
-      console.log(props)
       let currentMovie = props.movie;
 
       const saveMovie = () => {
